435ce4d6335e5004a03cafdcbd539846819cc1ad
Urs Langenegger <urslangenegger@gmail.com>
2022-08-15 15:12:17 +0200

    lots of playing, with property wrappers (State), \.self, and two-way binding

--- visual0/ContentView.swift
+++ visual0/ContentView.swift
@@ -8,11 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    //    var body: some View {
-    //        Text("Hello, world!")
-    //            .padding()
-    //    }
+    
     @State private var tapCount = 0
+    @State private var name = ""
+    
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
     
     var body: some View {
         
@@ -26,6 +27,15 @@ struct ContentView: View {
                 Button("Tap Count: \(tapCount)") {
                     tapCount += 1
                 }
+                Text("Enter your name:")
+                TextField("", text: $name)
+                Text("Your name is \(name)")
+                Picker("Select your student", selection: $selectedStudent) {
+                    ForEach(students, id: \.self) {
+                        Text($0)
+                    }
+                }
+                Text("You chose student \(selectedStudent)")
             }
             
         }
