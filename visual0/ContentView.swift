6890b6d9de22bac2c097b2fed1ba82e67f884f82
Urs Langenegger <urslangenegger@gmail.com>
2022-08-15 12:21:11 +0200

    Status before lunch. Button works

--- visual0/ContentView.swift
+++ visual0/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    //    var body: some View {
+    //        Text("Hello, world!")
+    //            .padding()
+    //    }
+    @State private var tapCount = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            
+            Form {
+                Section {
+                    Text("Hello, world!")
+                        .padding()
+                }
+                Button("Tap Count: \(tapCount)") {
+                    tapCount += 1
+                }
+            }
+            
+        }
+        .navigationSubtitle("SwiftUI")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
