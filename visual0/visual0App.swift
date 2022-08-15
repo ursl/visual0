5b976278dea4e057fd64dda2635d95c68a346969
Urs Langenegger <urslangenegger@gmail.com>
2022-08-15 10:18:54 +0200

    Initial Commit

---
+++ visual0/visual0App.swift
@@ -0,0 +1,17 @@
+//
+//  visual0App.swift
+//  visual0
+//
+//  Created by Urs Langenegger on 15.08.22.
+//
+
+import SwiftUI
+
+@main
+struct visual0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
